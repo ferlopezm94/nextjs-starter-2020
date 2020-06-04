1988163f1b18ed10556671309f8db563ec6ecb83
Fernando López <ferlopezm94@gmail.com>
2020-06-04 17:30:06 -0500

    build(css): add global styles support

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from 'next/app';
+
+import './../styles/global.css';
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default App;
