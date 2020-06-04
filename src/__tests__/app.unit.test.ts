331b8431403823e5a9ca2a8e17ba0baf1cd61aae
Fernando López <ferlopezm94@gmail.com>
2020-06-04 01:18:49 -0500

    chore(nextjs): add setup

--- src/__tests__/app.unit.test.ts
+++
@@ -1,5 +0,0 @@
-import { greetingUppercase } from './../app';
-
-test('formats the greeting correctly', () => {
-  expect(greetingUppercase('world')).toMatchSnapshot();
-});
--- src/app.ts
+++
@@ -1,5 +0,0 @@
-export const greetingUppercase = (name: string): string => {
-  const uppercase = `Hello ${name}!`.toUpperCase();
-  console.log(uppercase);
-  return uppercase;
-};
