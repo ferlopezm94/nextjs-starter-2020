22f08d260b16339b6e0b2a59df5a80d4e7945178
Fernando López <ferlopezm94@gmail.com>
2020-06-04 12:22:00 -0500

    chore(jest): add react support with nextjs compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  testMatch: ['**/__tests__/**/*.(e2e|unit).test.ts'],
+  testMatch: ['**/__tests__/**/*.(e2e|unit).test.ts?(x)'],
+  globals: {
+    // We must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires. you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.jest.json',
+    },
+  },
 };
